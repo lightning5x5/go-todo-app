24e7371e83466758fb61b389d4c5416369501b54
lightning5x5 <lightning5x5@gmail.com>
2024-04-08 20:25:01 +0900

    ✨ add: TODO の一覧を取得するエンドポイントを追加

--- main.go
+++ main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+    "time"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-    r := gin.Default()
+// int 型を基底型として Status 型を定義
+type Status int
 
-    r.GET("/", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "message": "Hello, World!",
-        })
-    })
+// Status の定数を定義
+const (
+    StatusPending   Status = 0
+    StatusCompleted Status = 1
+)
+
+// todo の構造体を定義
+type todo struct {
+    ID          string    `json:"id"`
+    Name        string    `json:"name"`
+    Description string    `json:"description"`
+    Status      Status    `json:"status"`
+    Duedate     time.Time `json:"duedate"`
+}
+
+// FIXME: 後で DB で管理するように直す
+// TODO の実データをスライスで定義
+var todos = []todo {
+    {ID: "1", Name: "買い物", Description: "卵、牛乳", Status: StatusCompleted, Duedate: time.Now().Add(24 * time.Hour)},
+    {ID: "2", Name: "読書", Description: "Go 入門", Status: StatusPending, Duedate: time.Now().Add(48 * time.Hour)},
+    {ID: "3", Name: "Gin のチュートリアル読む", Description: "https://go.dev/doc/tutorial/web-service-gin", Status: StatusPending, Duedate: time.Now().Add(72 * time.Hour)},
+}
+
+func getTodos(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, todos)
+}
+
+func main() {
+    r := gin.Default()  // Engine インスタンスを生成し、そのポインターを返す
+
+    // gin.Context はその HTTP リクエストに関する情報を表す構造体
+    // HTTP リクエスト処理時に自動的に渡される
+    // Laravel の Request みたいなやつ
+    r.GET("/todos", getTodos)
 
     r.Run()
 }
